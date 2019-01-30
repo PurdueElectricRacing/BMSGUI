766afb1849717375601af7d3576c35674f8a3565
raymonddong <raymonddonghome@gmail.com>
2019-01-30 16:10:45 -0500

    logs into database
    can receive messages form client to update can table

--- bmsgui/bmsgui.py
+++ bmsgui/bmsgui.py
@@ -1,55 +1,146 @@
 import design
 import socket
-import sqlite3
 import sys
-import time
+from constants import *
 from contextlib import closing
+from os import path
 from PyQt4 import QtCore, QtGui, QtSql
-from sqlite3 import Error
 
+@fdec(dec)
 class MyApp(QtGui.QMainWindow, design.Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         QtGui.QMainWindow.__init__(self)
         design.Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self.actionNew.triggered.connect(self.createDB)
-        #self.actionOpen.triggered.connect()
-        self.actionClose.triggered.connect(self.closeEvent)
+        self.connectdb = False
+        self.dbname = ''
+        self.db = None
+        self.tableCellinit()
+        self.tableTempinit()
+        self.tableModinit()
+        self.tableCANinit()
+        self.actionNew.triggered.connect(self.new)
+        self.actionOpen.triggered.connect(self.open)
+        self.actionClose.triggered.connect(self.close)
+        self.actionQuit.triggered.connect(self.closeEvent)
         #self.actionPreferences.triggered.connect()
+    
+    def tableCellinit(self, *args, **kwargs):
+        self.modelCell = QtGui.QStandardItemModel()
+        self.modelCell.setHorizontalHeaderLabels(headerCellh)
+        self.modelCell.setVerticalHeaderLabels(headerCellv)
+        rowCount, colCount = self.modelCell.rowCount(), self.modelCell.columnCount()
+        for row in range(rowCount):
+            for col in range(colCount -1):
+                self.modelCell.setItem(row, col, QtGui.QStandardItem('-'))
+            self.modelCell.setItem(row, colCount - 1, QtGui.QStandardItem('YYYY-mm-dd HH:MM:SS'))
+        self.tableCell.setModel(self.modelCell)
+        self.tableCell.horizontalHeader().setStretchLastSection(True)
+        self.tableCell.horizontalHeader().resizeSections(3)
+    
+    def tableTempinit(self, *args, **kwargs):
+        self.modelTemp = QtGui.QStandardItemModel()
+        self.modelTemp.setHorizontalHeaderLabels(headerTemph)
+        self.modelTemp.setVerticalHeaderLabels(headerTempv)
+        rowCount, colCount = self.modelTemp.rowCount(), self.modelTemp.columnCount()
+        for row in range(rowCount):
+            for col in range(colCount -1):
+                self.modelTemp.setItem(row, col, QtGui.QStandardItem('-'))
+            self.modelTemp.setItem(row, colCount - 1, QtGui.QStandardItem('YYYY-mm-dd HH:MM:SS'))
+        self.tableTemp.setModel(self.modelTemp)
+        self.tableTemp.horizontalHeader().setStretchLastSection(True)
+        self.tableTemp.horizontalHeader().resizeSections(3)
+    
+    def tableModinit(self, *args, **kwargs):
+        #soc soh i
+        pass
 
-    def createDB(self):
-        db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('bmsdata.db')
-        model = QtSql.QSqlTableModel()
-        delrow = -1
-        model.setTable('candata')
-        model.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
-        model.select()
-        model.setHeaderData(0, QtCore.Qt.Horizontal, "CAN ID")
-        model.setHeaderData(1, QtCore.Qt.Horizontal, "Data")
-        self.tableCAN.setModel(model)
-        print self.tableCAN
-        print "db"
+    def tableCANinit(self, *args, **kwargs):
+        self.modelCAN = QtGui.QStandardItemModel()
+        self.modelCAN.setHorizontalHeaderLabels(headerCANh)
+        self.modelCAN.setVerticalHeaderLabels(headerCANv)
+        rowCount, colCount = self.modelCAN.rowCount(), self.modelCAN.columnCount()
+        for row in range(rowCount):
+            for col in range(colCount -1):
+                self.modelCAN.setItem(row, col, QtGui.QStandardItem('0xXX'))
+            self.modelCAN.setItem(row, colCount - 1, QtGui.QStandardItem('YYYY-mm-dd HH:MM:SS'))
+        self.tableCAN.setModel(self.modelCAN)
+        self.tableCAN.horizontalHeader().setStretchLastSection(True)
+        self.tableCAN.horizontalHeader().resizeSections(3)
+    
+    def tableCANcom(self, msg, *args, **kwargs):
+        #echoclient
+        id = msg[1]
+        data = msg[2:] + msg[0]
+        if id in headerCANv:
+            row = headerCANv.index(id)
+            colCount = self.modelCAN.columnCount()
+            for col in range(colCount):
+                self.modelCAN.setItem(row, col, QtGui.QStandardItem(data[col]))
 
-
-    def openDB(self):
-        print "db"
-        if not db.open():
-            print "bd"
+    def tableCANall(self, *args, **kwargs):
         query = QtSql.QSqlQuery()
-        query.exec_("create table sportsmen(id int primary key, "
-            "firstname varchar(20), lastname varchar(20))")
-        query.exec_("insert into sportsmen values(101, 'Roger', 'Federer')")
-        query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
-        query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
-        query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
-        query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
-        print "db"
+        result = QtSql.QSqlResult()
+        colCount = self.modelCAN.columnCount()
+        for id in headerCANv:
+            row = headerCANv.index(id)
+            #query
+            for col in range(colCount):
+                self.modelCAN.setItem(row, col, QtGui.QStandardItem(data[col]))
 
-    def connect(self):
+    def new(self, *args, **kwargs):
+        text, ok = QtGui.QInputDialog.getText(self, 'New Database File', 'Enter Database Name:')
+        if ok:
+            if not path.exists(text + '.db'):
+                if self.db is not None:
+                    self.db.close()
+                self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+                self.dbname = text
+                self.db.setDatabaseName(self.dbname + '.db')
+                print self.dbname
+                if not self.db.open():
+                    QtGui.QMessageBox.critical(None, QtGui.qApp.tr("Cannot open database"), QtGui.qApp.tr(errorDBopen), QtGui.QMessageBox.Cancel)
+                    return
+                self.connectdb = True
+                query = QtSql.QSqlQuery()
+                query.exec_("create table candata(date text primary key, id text, b0 text, b1 text, b2 text, b3 text, b4 text, b5 text, b6 text, b7 text)")
+                print query.lastQuery()
+                for id in headerCANv:
+                    query.exec_("insert into candata values('1776-07-04 00:00:0" + str(headerCANv.index(id)) + "', '" + id+ "', '0x00', '0x00', '0x00', '0x00', '0x00', '0x00', '0x00', '0x00')")
+                    print query.lastQuery()
+            else:
+                QtGui.QMessageBox.critical(None, QtGui.qApp.tr("Cannot create database"), QtGui.qApp.tr(errorDBexist), QtGui.QMessageBox.Cancel)
+    
+    def open(self, *args, **kwargs):
+        self.dbname = str(QtGui.QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Database files (*.db)"))
+        if self.dbname is not None:
+            self.dbname = path.basename(self.dbname)[:-3]
+            if self.db is not None:
+                self.db.close()
+            self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+            self.db.setDatabaseName(self.dbname + '.db')
+            print self.dbname
+            if not self.db.open():
+                QtGui.QMessageBox.critical(None, QtGui.qApp.tr("Cannot open database"), QtGui.qApp.tr(errorDBopen), QtGui.QMessageBox.Cancel)
+                return
+            self.connectdb = True
+            #self.connect()
+        #self.tableCANcom(['0x0202', '0x00', '0x00', '0x00', '0x00', '0x00', '0x00', '0x00', '0x00', 'today'])
+    
+    def close(self, *args, **kwargs):
+        if self.db is not None:
+            self.db.close()
+        self.db = None
+        self.dbname = ''
+        self.tableCellinit()
+        self.tableTempinit()
+        self.tableModinit()
+        self.tableCANinit()
+
+    def connect(self, *args, **kwargs):
         with closing(socket.socket()) as s:
             self.HOST = socket.gethostname()
-            self.PORT = 1090
+            self.PORT = portNumber
             print "0\n"
             s.bind((self.HOST, self.PORT))
             s.listen(5)
@@ -60,15 +151,18 @@ class MyApp(QtGui.QMainWindow, design.Ui_MainWindow):
                     data = conn.recv(1024)
                     if data:
                         print data
+                        self.tableCANcom(data.split())
                     conn.sendall(data)
+                    if data:
+                        return
 
-    def closeEvent(self, event):
-        # closing
-        pass
+    def closeEvent(self, event, *args, **kwargs):
+        if self.db is not None:
+            self.db.close()
+        sys.exit(0)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MyApp()
     window.show()
     sys.exit(app.exec_())
-    #time.sleep(5)
---
+++ bmsgui/constants.py
@@ -0,0 +1,29 @@
+headerCellh = ['Voltage (V)', 'Internal Resistance (mOhm)', 'Date']
+headerCellv = ['Cell 00', 'Cell 01', 'Cell 02', 'Cell 03', 'Cell 04', 'Cell 05', 'Cell 06', 'Cell 07', 'Cell 08', 'Cell 09', 'Cell 10', 'Cell 11']
+headerTemph = ['Temperature (\xb0C)', 'Date']
+headerTempv = ['Site 0', 'Site 1', 'Site 2']
+headerCANh = ['Byte0', 'Byte1', 'Byte2', 'Byte3', 'Byte4', 'Byte5', 'Byte6', 'Byte7', 'Date']
+headerCANv = ['0x0202', '0x00A0', '0x00A1', '0x00A2']
+
+errorDBopen = """Unable to establish a database connection.\n
+                This example needs SQLite support. 
+                Please read the Qt SQL driver documentation for information how to build it.\n\n
+                Click Cancel to exit."""
+errorDBexist = """Database already exists"""
+
+portNumber = 1090
+
+def dec(func):
+    def wrapper(*args):
+        print "0 - " + func.__name__
+        func(*args)
+        print "1 - " + func.__name__
+    return wrapper
+
+def fdec(d):
+    def wrapper(cls):
+        for attr in cls.__dict__:
+            if callable(getattr(cls, attr)):
+                setattr(cls, attr, d(getattr(cls, attr)))
+        return cls
+    return wrapper
