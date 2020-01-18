c8207d84455b1a7bbc1dcabdcea8edcc57863dcb
chris <fallon2@purdue.edu>
2020-01-18 12:03:22 -0500

    pytalk works we can use electron and python

--- bmsgui/index.js
+++
@@ -1,9 +0,0 @@
-const pytalk = require('pytalk');
-
-var tester = pytalk.worker('./python_interface/communication.py');
-var test = tester.method('test');
-
-test('this some garbage yo', (err, retgarbage) => 
-{
-  console.log(retgarbage);
-});
