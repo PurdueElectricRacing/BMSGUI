6fc3af9480421425571972c97e5933d61fc9de7f
chris <fallon2@purdue.edu>
2020-01-18 14:49:14 -0500

    testing things

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const pytalk = require('pytalk');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 var tester = pytalk.worker('./python_interface/communication.py');
 var test = tester.method('test');
 
-test('this some garbage yo', (err, retgarbage) => 
-{
-  console.log(retgarbage);
+test('infinite loop test', (err) => {
+  
 });
+
+function createWindow()
+{
+  winder = new BrowserWindow(
+    {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    }
+  );
+  winder.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
+
+
+// test('this some garbage yo', (err, retgarbage) => 
+// {
+//   console.log(retgarbage);
+// });
--- python_interface/communication.py
+++ python_interface/communication.py
@@ -2,6 +2,6 @@
 
 @pytalk_method('test')
 def whatever(garbage):
-  original_garbage = garbage;
-  garbage = "somebody once told me the world was gonna roll me"
-  return original_garbage, garbage
+  while True:
+    print(garbage);
+    
