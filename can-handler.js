f57957039db073ddedc6498fe229b1dd202e22a2
Tyler Marchok <tylermarchok@pal-nat186-102-227.itap.purdue.edu>
2020-01-23 19:40:27 -0500

    init CAN msg handler

---
+++ can-handler.js
@@ -0,0 +1,63 @@
+//const SerialPort = require('serialport')   INCL. in CANPort
+let CANPort = require('./index').CANPort;
+
+var can;
+var port;
+var glob = require("glob")
+
+var known_messages = {'0x605': 'volt_id',
+'0x606': 'temp_msg',
+'0x608': 'resistance_msg',
+'0x609': 'error_msg'
+}
+
+function recieve(){
+    while(true){
+        var message = can.recv();
+        var id = message.id();
+        var data = message.data();
+
+        if (known_messages.indexOf(id) >= 0){
+            return (id, data);
+
+        }
+        else{
+            console.log('message unknown');
+        }
+    }
+}
+
+
+
+
+function init_connection(){
+
+        platform = process.platform;
+
+         if platform.startswith('win'){
+            ports = ['COM%s' % (i + 1) for i in range(256)];
+         }
+         else if platform.startswith('linux') or platform.startswith('cygwin'){
+            ports = glob("/dev/tty[A-Za-z]*")
+         }
+         else if platform.startswith('darwin'){
+            ports = glob.glob('/dev/tty.*');
+            ports = glob("/dev/tty.*")
+         }
+         else:
+            console.log('Unsupported platform');
+
+         for (var index in ports){
+            if (index !== null)
+                port = index
+         }
+
+
+//      console.log(await CANPort.listSerialPorts());
+        let can = new CANPort(port);
+        can.open();
+        can.setBitRate(125000);
+        can.on('data', console.log);
+}
+
+}
--- python_interface/communication.py
+++ python_interface/communication.py
@@ -1,7 +1,7 @@
-# import sys
-# import os
-# import serial
-# import glob
+import sys
+import os
+import serial
+import glob
 # import threading
 # import gc
 # import time
@@ -13,11 +13,11 @@ emitSocket = SocketIO('localhost', 80085, LoggingNamespace)
 # port = None
 return_msg = 0
 
-# messages = {'0x605': 'volt_id',
-# '0x606': 'temp_msg',
-# '0x608': 'resistance_msg',
-# '0x609': 'error_msg'
-# }
+messages = {'0x605': 'volt_id',
+'0x606': 'temp_msg',
+'0x608': 'resistance_msg',
+'0x609': 'error_msg'
+}
 
 
 def getData():
@@ -25,19 +25,19 @@ def getData():
 
 
 # @pytalk_method('findport')
-# def findPort():
-#     if sys.platform.startswith('win'):
-#       ports = ['COM%s' % (i + 1) for i in range(256)]
-#     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-#       ports = glob.glob('/dev/tty[A-Za-z]*')
-#     elif sys.platform.startswith('darwin'):
-#       ports = glob.glob('/dev/tty.*')
-#     else:
-#       raise EnvironmentError('Unsupported platform')
+def findPort():
+    if sys.platform.startswith('win'):
+      ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+      ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+      ports = glob.glob('/dev/tty.*')
+    else:
+      raise EnvironmentError('Unsupported platform')
 
-#     for index in ports:
-#       if index is not None:
-#         port = index
+    for index in ports:
+      if index is not None:
+        port = index
 
 
 @pytalk_method('run')
@@ -49,10 +49,10 @@ def run():
   # bus.start()
   print('huh');
   while True:
-    # print(dev.recv())
-    # msg = bus.recv()
-    # msg_start, msg_end = int(_[37], base=16), int(_[64], base=16)
-    # data = msg[msg_start:msg_end]
+    print(dev.recv())
+    msg = bus.recv()
+    msg_start, msg_end = int(_[37], base=16), int(_[64], base=16)
+    data = msg[msg_start:msg_end]
     # if data in messages.keys():
         # return_val = (data, messages.get(data))
     # return_msg = 'help'
